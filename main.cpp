0f2d4c4193e90c5f4d74105bb097e4859cf91e5c
ANIMO-07 <b19069@students.iitmandi.ac.in>
2021-08-23 18:41:36 +0530

    Step-6

--- main.cpp
+++ main.cpp
@@ -6,4 +6,9 @@ int main(){
     cout<< endl;
     cout<<"The factorial of 5 is "<< factorial(5)<< endl;
     return 0;
+
+	int x = 56;
+	int y = 98;
+	cout<<"\nThe gcd of 56 and 98 is "<< gcd(x, y);
+    return 0;
 }
